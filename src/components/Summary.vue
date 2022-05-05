db3b008a167b9af189deff82446e015174f850ba
michael beloff <michael.a.beloff@gmail.com>
2022-05-05 16:01:59 +1000

    show more price details

--- src/components/Summary.vue
+++ src/components/Summary.vue
@@ -1,53 +1,84 @@
 <template>
   <div
-    class="mx-auto flex w-full max-w-screen-md flex-col justify-between rounded bg-gray-100"
+    class="mx-auto flex w-full max-w-screen-md flex-col justify-between rounded bg-gray-100 py-4 text-sm"
   >
-    <p class="p-2 font-bold">
+    <p class="mb-5 font-bold">
       Booking: {{ bookinginfo.bookinginfo[0].reservationdocumentno }}
     </p>
-    <p>
+    <p class="mb-5">
       {{ trip.vehiclecategory }}
     </p>
 
-    <div class="my-2 grid grid-cols-2 gap-x-2 rounded p-2 text-left">
-      <p class="text-right font-bold">Picking up:</p>
-      <p>
-        {{ trip.pickuplocationname }} <br />
-        {{ trip.pickupdate + " - " + timeConvert(trip.pickuptime) }}
-      </p>
-      <p class="text-right font-bold">Droping off:</p>
-      <p>
-        {{ trip.dropofflocationname }} <br />
-        {{ trip.dropoffdate + " - " + timeConvert(trip.dropofftime) }}
-      </p>
+    <div class="flex items-center font-bold">
+      <span class="flex-grow text-right">{{ trip.pickuplocationname }}</span>
+      <i class="fas fa-route fa-fw mx-4 text-lg text-blue-500"></i>
+      <span class="flex-grow text-left">{{ trip.pickuplocationname }}</span>
+    </div>
+    <div class="mb-5 flex items-center">
+      <span class="flex-grow text-right">{{
+        new Date(trip.pickupdate).toDateString()
+      }}</span>
+      <i class="fas fa-minus fa-fw invisible mx-4 text-lg text-blue-500"></i>
+      <span class="flex-grow text-left">{{
+        new Date(trip.dropoffdate).toDateString()
+      }}</span>
     </div>
 
-    <!-- <div class="flex justify-between rounded-b bg-gray-200 py-2 font-bold px-2">
-      <span>Balance Due:</span
-      ><span
-        >{{ trip.currencyname + trip.currencysymbol
-        }}{{ trip.balancedue }}</span
+    <div class="mx-auto mb-5 w-full max-w-[400px] px-2 text-xs">
+      <div class="flex justify-between">
+        <span>{{
+          rate.numberofdays +
+          " days @ " +
+          trip.currencysymbol +
+          rate.dailyrateafterdiscount +
+          "/day"
+        }}</span>
+        <span>{{ trip.currencysymbol + rate.ratesubtotal }}</span>
+      </div>
+      <div
+        class="flex justify-between"
+        v-for="item in fees"
+        :key="item.extrafeeid"
       >
-    </div> -->
+        <span>{{ item.name }}</span
+        ><span>{{ trip.currencysymbol + item.totalfeeamount }}</span>
+      </div>
+      <div class="mt-4 flex justify-between">
+        <span>Total</span><span>{{ trip.totalcost }}</span>
+      </div>
+      <p class="text-right mt-1">
+        (includes GST of: {{ trip.currencysymbol + trip.gst }})
+      </p>
+    </div>
+    <div class="bg-gray-200">
+      <div
+        class="mx-auto flex w-full max-w-[400px] justify-between py-5 px-2 font-bold"
+      >
+        <span>Balance Due:</span
+        ><span
+          >{{ trip.currencyname + trip.currencysymbol
+          }}{{ trip.balancedue }}</span
+        >
+      </div>
+    </div>
   </div>
 </template>
 
+<script setup>
+const props = defineProps({
+  bookinginfo: {
+    type: Object,
+    default: null,
+  },
+});
+
+const trip = props.bookinginfo.bookinginfo[0];
+const fees = props.bookinginfo.extrafees;
+const rate = props.bookinginfo.rateinfo[0];
+</script>
+
 <script>
-// import Mixins from "@/Mixins.js";
 export default {
-  name: "Summary",
-  props: {
-    bookinginfo: {
-      type: Object,
-      required: true,
-    },
-  },
-  // Mixins: [Mixins],
-  computed: {
-    trip() {
-      return this.bookinginfo.bookinginfo[0];
-    },
-  },
   // mounted() {
   //   let params = {
   //     method: "workflowchecklist",
@@ -56,22 +87,6 @@ export default {
   //   };
   //   Mixins.methods.postapiCall(params).then((res) => console.log(res));
   // },
-  methods: {
-    timeConvert(time) {
-      // Check correct time format and split into components
-      time = time
-        .toString()
-        .match(/^([01]\d|2[0-3])(:)([0-5]\d)(:[0-5]\d)?$/) || [time];
-
-      if (time.length > 1) {
-        // If time format correct
-        time = time.slice(1); // Remove full string match value
-        time[5] = +time[0] < 12 ? "AM" : "PM"; // Set AM/PM
-        time[0] = +time[0] % 12 || 12; // Adjust hours
-      }
-      return time.join(""); // return adjusted time or original string
-    },
-  },
 };
 </script>
 
