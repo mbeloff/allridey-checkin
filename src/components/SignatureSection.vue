c1faf9177c9f79ba656c05ef557f763d809da22d
michael beloff <michael.a.beloff@gmail.com>
2022-05-06 12:08:15 +1000

    refactor (composition api)

--- src/components/SignatureSection.vue
+++ src/components/SignatureSection.vue
@@ -10,54 +10,92 @@
   </div>
 </template>
 
-<script>
-import Mixins from "@/Mixins.js";
-import LoadingOverlay from "@/components/LoadingOverlay.vue";
+<script setup>
 import SignatureBlock from "@/components/SignatureBlock.vue";
+import { inject, onBeforeMount } from "vue";
+import { useStore } from "@/store";
+
+const rcm = inject("rcm");
+
+const store = useStore();
+
+const props = defineProps({
+  cid: Number,
+});
+
+const signatureList = ref([]);
+
+const missing = computed(() => {
+  return signatureList.value.filter((sig) => !sig.issigned).length;
+});
+
+function getSignatures() {
+  let params = {
+    method: "signaturelist",
+    reservationref: store.resref,
+  };
+  rcm(params)
+    .then((res) => {
+      signatureList.value = res.results.filter(
+        (el) => el.customerid == props.cid
+      );
+    })
+    .catch((err) => console.log(err));
+}
+
+onBeforeMount(() => {
+  getSignatures();
+});
+</script>
+
+<script>
+// import Mixins from "@/Mixins.js";
+// import LoadingOverlay from "@/components/LoadingOverlay.vue";
+// import SignatureBlock from "@/components/SignatureBlock.vue";
 export default {
-  props: {
-    cid: { type: Number, required: true },
-  },
-  emits: ["update-section-status"],
-  components: {
-    LoadingOverlay,
-    SignatureBlock,
-  },
-  data() {
-    return {
-      signatureList: [],
-    };
-  },
-  mixins: [Mixins],
-  beforeMount() {
-    this.getSignatures();
-  },
-  watch: {
-    signatureList: function (val) {
-      let missing = false;
-      val.forEach((el) => {
-        if (!el.issigned) {
-          missing = true;
-        }
-      });
-      this.$emit("update-section-status", missing);
-    },
-  },
+  // props: {
+  //   cid: { type: Number, required: true },
+  // },
+  // emits: ["update-section-status"],
+  // components: {
+  //   LoadingOverlay,
+  //   SignatureBlock,
+  // },
+  // data() {
+  //   return {
+  //     signatureList: [],
+  //   };
+  // },
+  // mixins: [Mixins],
+  // beforeMount() {
+  //   this.getSignatures();
+  // },
+  // watch: {
+  //   signatureList: function (val) {
+  //     let missing = false;
+  //     val.forEach((el) => {
+  //       if (!el.issigned) {
+  //         missing = true;
+  //       }
+  //     });
+  //     this.$emit("update-section-status", missing);
+  //   },
+  // },
   methods: {
-    getSignatures() {
-      let params = {
-        method: "signaturelist",
-        reservationref: this.$store.state.resref,
-      };
-      Mixins.methods
-        .postapiCall(params)
-        .then((res) => {
-          this.signatureList = res.results.filter(
-            (el) => el.customerid == this.cid
-          );
-        })
-        .catch((err) => console.log(err));
-    },
+    // getSignatures() {
+    //   let params = {
+    //     method: "signaturelist",
+    //     reservationref: this.$store.state.resref,
+    //   };
+    //   Mixins.methods
+    //     .postapiCall(params)
+    //     .then((res) => {
+    //       this.signatureList = res.results.filter(
+    //         (el) => el.customerid == this.cid
+    //       );
+    //     })
+    //     .catch((err) => console.log(err));
+    // },
   },
 };
 </script>
