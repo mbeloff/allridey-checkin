9f48be6043556d801a340d90d811856e67c2395d
michael beloff <michael.a.beloff@gmail.com>
2022-05-23 14:18:30 +1000

    tidy up

--- src/main.js
+++ src/main.js
@@ -5,7 +5,6 @@ import { DatePicker } from "v-calendar";
 import { routes } from "./routes.js";
 import { createRouter, createWebHistory } from "vue-router";
 import VueSignaturePad from "vue-signature-pad";
-import Vue3Signature from "vue3-signature"
 import { createPinia } from "pinia";
 
 const app = createApp(App);
@@ -19,6 +18,5 @@ app
   .use(router)
   .use(createPinia())
   .use(VueSignaturePad)
-  .use(Vue3Signature)
   .component("DatePicker", DatePicker);
 app.mount("#app");
