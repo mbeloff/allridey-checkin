92336d02a9d91c5dea93aa147fdd433fbc44043a
michael beloff <michael.a.beloff@gmail.com>
2022-05-23 14:13:16 +1000

    revert to options api for signature component

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import { DatePicker } from "v-calendar";
 import { routes } from "./routes.js";
 import { createRouter, createWebHistory } from "vue-router";
 import VueSignaturePad from "vue-signature-pad";
+import Vue3Signature from "vue3-signature"
 import { createPinia } from "pinia";
 
 const app = createApp(App);
@@ -18,5 +19,6 @@ app
   .use(router)
   .use(createPinia())
   .use(VueSignaturePad)
+  .use(Vue3Signature)
   .component("DatePicker", DatePicker);
 app.mount("#app");
