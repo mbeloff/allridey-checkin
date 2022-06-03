5d2f9af1efbf183cd71fe20e32e243b7f6d97dfd
michael beloff <michael.a.beloff@gmail.com>
2022-06-03 14:54:05 +1000

    change message for allocated/unallocated status

--- src/components/ModifySignatures.vue
+++ src/components/ModifySignatures.vue
@@ -3,15 +3,14 @@
     <p class="col-span-full my-3 text-xl font-bold text-green-500">
       {{ fullname }}
     </p>
-    <p class="my-3 text-sm text-gray-500" v-if="store.status == 'Reservation'">
+    <p class="my-3 text-sm text-gray-500" v-if="store.allocated">
       Please read and sign each required area using your mouse, or finger/stylus
       if you are using a touch-screen device.
     </p>
-    <p
-      class="my-3 text-sm font-bold text-orange-500"
-      v-if="store.status == 'Reservation Request'"
-    >
-      <i class="far fa-warning"></i> Please return here once your booking has been confirmed.
+    
+    <p class="my-3 text-sm font-bold text-orange-500" v-if="!store.allocated">
+      <i class="far fa-warning"></i> Please return here once your booking has
+      been allocated.
     </p>
 
     <div
